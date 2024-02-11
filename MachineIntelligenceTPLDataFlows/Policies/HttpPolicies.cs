5b926093daaf0c27423b4cfe5874ad5a3d7595b5
Bart Czernicki <bartczernicki@outlook.com>
2024-02-11 18:04:25 -0500

    Added Polly code for retry logging

--- MachineIntelligenceTPLDataFlows/Policies/HttpPolicies.cs
+++ MachineIntelligenceTPLDataFlows/Policies/HttpPolicies.cs
@@ -1,4 +1,5 @@
-﻿using Polly;
+﻿using Microsoft.Extensions.Logging;
+using Polly;
 using Polly.Extensions.Http;
 using System;
 using System.Net.Http;
@@ -11,8 +12,15 @@ namespace MachineIntelligenceTPLDataFlows.Policies
         {
             var retryPolicy = HttpPolicyExtensions
             .HandleTransientHttpError() // HttpRequestException, 5XX and 408
+            .OrResult(response => response.StatusCode == System.Net.HttpStatusCode.NotFound) //  Handle NotFound 404
             .OrResult(response => response.StatusCode == System.Net.HttpStatusCode.TooManyRequests) //  Handle too many requests 429
-            .WaitAndRetryAsync(4, retryAttempt => TimeSpan.FromSeconds(Math.Pow(3, retryAttempt)));
+            .WaitAndRetryAsync(4, retryAttempt => TimeSpan.FromSeconds(Math.Pow(3, retryAttempt)),
+                    onRetry: (response, calculatedWaitDuration) =>
+                    {
+                        Console.WriteLine($"Failed attempt. Waited for {calculatedWaitDuration}");// Retrying. {response.Exception.Message} - {response.Exception.StackTrace}");
+                    }
+            );
+            
 
             return retryPolicy;
         }
